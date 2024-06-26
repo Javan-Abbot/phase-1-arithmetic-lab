eb19bba0fbd44e74b15a108319b2577e50583fde
+javan abbot <+>
2024-06-26 12:37:20 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let multiply;
+let num1=2;
+let num2=31;
+multiply=num1*num2;
+
+
+let random;
+random=1;
+Math.random(random);
+
+let mod;
+let num3=16;
+let num4=6;
+mod=num3%=6;
+
+
+let max=(1,2,4,50,50,22,20);
+Math.max(max);
+
+
